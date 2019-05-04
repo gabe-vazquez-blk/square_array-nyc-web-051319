fc1a2bb127e64031b260a0309a2e6399238fb4bf
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-05-04 14:39:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,14 +1,9 @@
-#def square_array(array)
-  # your code here
-  #a=[]
-  #array.each do |square| 
-    #square = square**2
-    #a.push(square)
-  #end
-  #a
-#end
-
 def square_array(array)
-  array.collect { |x| x**2 }  
+  # your code here
+  a=[]
+  array.each do |square| 
+    square = square**2
+    a.push(square)
   end
+  a
 end
